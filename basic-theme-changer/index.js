86da495f42d85717712255a14ff1cbfe43b93f77
sophia <sophieoyiza@gmail.com>
2021-07-23 21:34:54 +0100

    fix: :bug: section theme add
    section theme add
    
    none

--- basic-theme-changer/index.js
+++ basic-theme-changer/index.js
@@ -1,14 +1,18 @@
 // access the body element node in html and store it 
 let body = document.body.style
+
+let section = document.getElementById("section").style
 // function that has default,  another function for dark theme
 
 const defaultTheme = ()=> {
 // change the bgColor and font color values of the body element 
 body.backgroundColor = "#fff"
 body.color = "#000"
+section.backgroundColor = "grey"
 }
 const darkTheme = () => {
     body.backgroundColor = "#000"
     body.color = "#fff"
+    section.backgroundColor = "#000"
 }
 // call the function .... event listener: onclick... call it in html
