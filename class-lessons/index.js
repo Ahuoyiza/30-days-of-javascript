e1fab61a48ebbfad3af5919a83c1d2a6e4a13662
sophia <sophieoyiza@gmail.com>
2021-08-25 20:33:51 +0100

    feat: :sparkles: adds task
    adds task
    
    none

---
+++ class-lessons/index.js
@@ -0,0 +1,17 @@
+// numbers
+// Math.floor(x)
+let sum = 10.3 + 3.9999999;
+console.log(Math.floor(sum));
+let newSum =Math.floor(sum)
+let color = 255;
+console.log(color.toString(16));
+
+// base 2, base 10, base 16
+// hexcode #fff 255 (000 to 255)
+// parseInt() parseFloat()
+let string = "100.9m";
+console.log(typeof(string));// string
+let number= parseFloat(string);
+console.log(number);// number
+
+//
